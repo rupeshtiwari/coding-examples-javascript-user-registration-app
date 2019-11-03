10112b872531afd7dd3f23c1dc0506e2e5617b43
Rupesh Kumar Tiwari <roopkt@gmail.com>
2019-11-02 21:58:41 -0400

    preparing for heroku

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,18 +1,12 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-const path = require('path');
+
 module.exports = {
   entry: './src/app/index.js',
   output: {
     filename: 'bundle.js'
   },
-  watch: true,
-  devServer: {
-    contentBase: path.join(__dirname, 'dist'),
-    compress: true,
-    port: 9000,
-    open: true
-  },
+  mode: 'production',
   plugins: [
     // new CleanWebpackPlugin(['dist/*']) for < v2 versions of CleanWebpackPlugin
     new CleanWebpackPlugin(),
